54ad52170a8e694134314447e828fc9638e06765
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2025-01-02 15:21:44 +0100

    small fixes

--- shaders/bulb_frag.glsl
+++ shaders/bulb_frag.glsl
@@ -86,6 +86,7 @@ vec3 getColor(float dist, vec3 pos) {
 	color = vec3(0.5 + 0.5 * cos(3.0 + dist * 10.0),
 					0.5 + 0.5 * cos(3.0 + dist * 10.0 + 2.0),
 					0.5 + 0.5 * cos(3.0 + dist * 10.0 + 4.0));
+
     return color;
 }
 
